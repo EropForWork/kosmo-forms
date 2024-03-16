8bee1c93e6b169f5f7c7651fcc841fdbd639581b
Erop <u4iha1@gmail.com>
2024-03-16 21:48:07 +0300

    fix: Сделать только один выбор.

--- src/Modules/Option-container.js
+++ src/Modules/Option-container.js
@@ -1,12 +1,12 @@
 import React from "react";
 
 const OptionContainer = ({ label }) => {
-  return (
-    <div className="option-container">
-      <input type="checkbox" />
-      <span className="option-label">{label}</span>
-    </div>
-  );
+    return (
+        <div className="option-container">
+            <input type="radio" name="option" />
+            <span className="option-label">{label}</span>
+        </div>
+    );
 };
 
 export default OptionContainer;
